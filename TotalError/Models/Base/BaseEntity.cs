2e16f0f1f5abec906ba2a2950e644070e8709401
Yanka Domenikova <yankadomenikova@gmail.com>
2022-01-02 21:15:15 +0200

    Add models

---
+++ TotalError/Models/Base/BaseEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Base
+{
+    public class BaseEntity : IEntity
+    {
+        public BaseEntity()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+
+        public string Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime DeletedAt { get; set; }
+        public string DeletedBy { get; set; }
+    }
+}
