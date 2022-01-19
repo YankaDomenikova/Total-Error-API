c40de83586cadf4fe0079f1ce30227214b4323f3
Yanka Domenikova <yankadomenikova@gmail.com>
2022-01-19 14:02:09 +0200

    Add Utilities

---
+++ TotalError/src/Data/Data.Utilities/Interfaces/IBaseCRUD.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Utilities.Interfaces
+{
+    public interface IBaseCRUD
+    {
+        public void ReadFile(string dir);
+    }
+}
