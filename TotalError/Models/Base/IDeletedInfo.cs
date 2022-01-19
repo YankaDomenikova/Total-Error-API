34c255509e8f92f9761ff44cbb812fd0e838d4a9
Yanka Domenikova <yankadomenikova@gmail.com>
2022-01-19 17:26:17 +0200

    Add Models

---
+++ TotalError/Models/Base/IDeletedInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Base
+{
+    public interface IDeletedInfo
+    {
+        public DateTime DeletedAt { get; set; }
+        public string DeletedBy { get; set; }
+        public bool isDeleted { get; set; }
+    }
+}
---
+++ TotalError/Models/Entities/Country.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Models.Base;
+
+namespace Models.Entities
+{
+    public class Country : BaseEntity
+    {
+        public string CountryName { get; set; }
+        public string RegionId { get; set; }
+        public Region Region { get; set; }
+        public List<Order> Orders { get; set; }
+    }
+}
