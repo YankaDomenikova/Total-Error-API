efe37dedc2911d45494f54c44dfabbc573aa9618
Yanka Domenikova <yankadomenikova@gmail.com>
2022-01-19 12:52:56 +0200

    Edit Models

---
+++ TotalError/src/Data/Data.Models/Entities/LastReadedFile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Data.Models.Base;
+
+namespace Data.Models.Entities
+{
+    public class LastReadedFile : BaseEntity
+    {
+        public DateTime LastReaded { get; set; }
+    }
+}
