57707e587c749ca480ef4f841420d68c0239437f
Yanka Domenikova <yankadomenikova@gmail.com>
2022-01-19 14:52:00 +0200

    Add ReadFile method

---
+++ TotalError/src/Test/Test.ConsoleApplication/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using Data.AppDbContext;
+using Data.Services.Implementations;
+using Data.Utilities.Constants;
+
+namespace Test.ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseCRUDService baseCRUD = new BaseCRUDService(new ApplicationDbContext());
+            baseCRUD.ReadFile(DirectoryConstant.Directory);
+        }
+    }
+}
